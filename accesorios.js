db8fb55d43be66ec5778436ec837a4f59a7a4505
buenapilcha <buenapilcha.com@gmail.com>
2026-06-27 18:03:19 -0300

    Add files via upload

---
+++ accesorios.js
@@ -0,0 +1,14 @@
+const accesorios = [
+    {
+        id: "acc-001",
+        name: "20W USB-C Power Adapter",
+        price: "$105.000",
+        imagen: "",
+        images: [
+            "https://acdn-us.mitiendanube.com/stores/006/244/960/products/cargador-1df508d309090d4dd317701537956408-1024-1024.webp",
+            "https://acdn-us.mitiendanube.com/stores/006/244/960/products/apple-cargador-usb-c-de-20-w-6e3dcbd5c9be6b64dd17537112479000-1024-1024.webp",
+            "https://acdn-us.mitiendanube.com/stores/006/244/960/products/apple-cargador-usb-c-de-20-w-6e3dcbd5c9be6b64dd17537112479000-1024-1024.webp"
+        ],
+        specs: ["Potencia de 20W","Conector USB-C" ,"Compatibilidad amplia", "Sin cable usb"]
+    }
+];
---
+++ iphone.js
@@ -0,0 +1,25 @@
+const iphone = [
+    {
+        id: "iph-001",
+        name: "IPHONE 13 USADOS",
+        price: "$500.000",
+        imagen: "https://via.placeholder.com/300/333333/ffffff?text=iPhone+13+Usado",
+        images: [
+            "https://acdn-us.mitiendanube.com/stores/001/116/601/products/13-pro-graphito-02-4d7156fb3903e90b7e17565749879753-1024-1024.webp",
+            "https://acdn-us.mitiendanube.com/stores/001/116/601/products/img_0109-1-7b3582611f542c184c17553677761804-1024-1024.webp",
+            "https://acdn-us.mitiendanube.com/stores/001/116/601/products/img_0105-1-b1b68a15bf152f6e0217553677761750-1024-1024.webp"
+        ],
+        specs: ["Estado: Grado A", "Batería: 100%", "Color: Graphito", "Incluye cable","512 gb"]
+    },
+    {
+        id: "iph-002",
+        name: "IPHONE 17 PRO MAX NUEVOS/SELLADOS",
+        price: "$3.500.000",
+        images: [
+            "https://maximstore.com/_next/image?url=https%3A%2F%2Fback.maximstore.com%2Fstatic%2Fimages%2Fbb6fb4f7-7537-4e03-ba34-acc1d85fa426.png&w=1280&q=75",
+            "https://maximstore.com/_next/image?url=https%3A%2F%2Fback.maximstore.com%2Fstatic%2Fimages%2F997e425c-8392-48c1-a236-94391e7e3dbf.png&w=1280&q=75",
+            "https://maximstore.com/_next/image?url=https%3A%2F%2Fback.maximstore.com%2Fstatic%2Fimages%2F3a91c13c-17f2-4b5c-8aba-1901b8d75bf8.jpeg&w=1280&q=75"
+        ],
+        specs: ["Estado: Nuevo sellado", "Garantía: 1 año Apple", "Color: Orange", "Memoria: 512GB"]
+    }
+];
